ecc73528a9f0509baa8da9b7954d93ceccd5a365
Oliver <oliveroide53@outlook.com>
2025-03-31 20:23:34 -0700

    Se implemento al usuario y corre feon perro corre

--- normalProgram.py
+++ normalProgram.py
@@ -31,13 +31,9 @@ while conter < 17:
             if i == 'A':
                 conter -= 10
                 break
-if conter > 21:
-    print("dealerDeck", dealerDeck)
-    print("this is dealer conter",conter)
-    print("dealer lose")
-    exit()
-print("dealerDeck", dealerDeck)
-print("this is dealer conter",conter) 
+print("dealerDeck", dealerDeck[0:1])
+#prueba conter dealer
+#print("this is dealer conter",conter) 
 dealerconter = conter
 array = []
 conter = 0 
@@ -57,7 +53,7 @@ if conter == 21:
 print("yourDeck", yourDeck)
 print("this is conter in for you",conter)   
 you = input("get a card Y, not get a card N ")
-if you == 'Y':
+while you == 'Y':
     rando = random.randint(0, 12)
     yourDeck.append(carts[rando])
     if rando == 0:
@@ -68,12 +64,22 @@ if you == 'Y':
     print("this is conter in new card",conter)
     print("yourDeck", yourDeck)    
     if conter > 21:
-        print("you lose")
-elif you == 'N':
-    print("you not get a card")
-    if dealerconter > conter:
-        print("dealer win")
-    elif dealerconter < conter:
-        print("you win")        
+        break
+    you = input("get a card Y, not get a card N ")    
+print("this is dealerconter",dealerconter)     
+##
+if conter > 21:
+    print("you lost")
+elif conter == dealerconter:
+    print("draw")
+elif dealerconter > 21:
+    print("dealerDeck", dealerDeck)
+    print("this is dealer conter",conter)
+    print("dealer lose")
+    exit()    
+if dealerconter > conter:
+    print("dealer win")
+elif dealerconter < conter:
+    print("you win")        
 
 
